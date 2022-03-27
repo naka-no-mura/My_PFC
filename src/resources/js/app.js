cd60ddf65a870212b3b709017d9774d932cafc3b
naka-no-mura <nakanomura.ryu@gmail.com>
2022-03-27 18:14:08 +0900

    vueの読み込み設定

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,1 +1,23 @@
-require('./bootstrap');
+require("./bootstrap");
+
+import { createApp } from "vue";
+import Sample from "./components/Sample.vue";
+
+createApp({
+    components: {
+        Sample,
+    },
+}).mount("#app");
+
+// import "./bootstrap";
+// import Vue from "vue";
+// import Sample from "./components/Sample.vue";
+
+// Vue.component("sample-component", require("./components/Sample.vue").default);
+
+// const app = new Vue({
+//     el: "#app",
+//     components: {
+//         Sample,
+//     },
+// });
